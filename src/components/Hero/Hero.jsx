e3eb4177329eab373e6ba83e29fe560c3d0e715a
Justin Pinkard <136210811+jdpinkard@users.noreply.github.com>
2023-11-30 16:22:33 -0600

    Added Resume Button and Resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getImageURL } from '../../utils'
 import styles from "./Hero.module.css"
+import resume from "../../data/Justin_Pinkard_resume.pdf";
 
 const Hero = () => {
   return (
@@ -8,7 +9,10 @@ const Hero = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Justin</h1>
             <p className={styles.description}>I'm a full-stack developer with 2 months of experience using React, HTML, and CSS. Reach out if you'd like to learn more!</p>
-            <a href="mailto:justin.d.pinkard@gmail.com" className={styles.contactBtn}>Contact Me</a>
+            <div className={styles.buttons}>
+            <a href="mailto:justin.d.pinkard@gmail.com" className={styles.textBtn}>Contact Me</a>
+            <a href={resume} target="_blank" className={styles.textBtn}>Resume</a>
+            </div>
         </div>
         <img src={getImageURL("hero/heroImage.png")} alt="hero image of Justin" className={styles.heroImg} />
         <div className={styles.topBlur} />
