1d40f358f5a74d7bc13d24ec21bb2799217f7106
Justin Pinkard <136210811+jdpinkard@users.noreply.github.com>
2023-11-29 19:28:06 -0600

    added Figma credit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,24 +5,27 @@ import styles from "./Contact.module.css"
 const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
-        <div className={styles.text}>
-            <h2>Contact</h2>
-            <p>Feel free to reach out!</p>
+        <div className={styles.subContainer}>
+            <div className={styles.text}>
+                <h2>Contact</h2>
+                <p>Feel free to reach out!</p>
+            </div>
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <a href='mailto:justin.d.pinkard@gmail.com'><img src={getImageURL("contact/emailIcon.png")} alt="Email Icon" /></a>
+                    <a href='mailto:justin.d.pinkard@gmail.com'>justin.d.pinkard@gmail.com</a>
+                </li>
+                <li className={styles.link}>
+                    <a href='https://www.linkedin.com/in/jdpinkard/'><img src={getImageURL("contact/linkedinIcon.png")} alt="LinkedIn Icon" /></a> 
+                    <a href='https://www.linkedin.com/in/jdpinkard/'>linkedin.com/in/jdpinkard</a>
+                </li>
+                <li className={styles.link}>
+                    <a href='https://github.com/jdpinkard'><img src={getImageURL("contact/githubIcon.png")} alt="Github Icon" /></a>
+                    <a href='https://github.com/jdpinkard'>github.com/jdpinkard</a>
+                </li>
+            </ul>
         </div>
-        <ul className={styles.links}>
-            <li className={styles.link}>
-                <a href='mailto:justin.d.pinkard@gmail.com'><img src={getImageURL("contact/emailIcon.png")} alt="Email Icon" /></a>
-                <a href='mailto:justin.d.pinkard@gmail.com'>justin.d.pinkard@gmail.com</a>
-            </li>
-            <li className={styles.link}>
-                <a href='https://www.linkedin.com/in/jdpinkard/'><img src={getImageURL("contact/linkedinIcon.png")} alt="LinkedIn Icon" /></a> 
-                <a href='https://www.linkedin.com/in/jdpinkard/'>linkedin.com/in/jdpinkard</a>
-            </li>
-            <li className={styles.link}>
-                <a href='https://github.com/jdpinkard'><img src={getImageURL("contact/githubIcon.png")} alt="Github Icon" /></a>
-                <a href='https://github.com/jdpinkard'>github.com/jdpinkard</a>
-            </li>
-        </ul>
+        <a href='https://www.figma.com/file/tkDs3aeBaqvF47hD063LSH/Untitled?type=design&node-id=0-1&mode=design&t=5ioyZdcqkmRG6uy7-0'>Based on a Figma Design</a>
     </footer>
   )
 }
